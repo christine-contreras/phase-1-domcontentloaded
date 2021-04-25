edf992faf8c2ebbc776aadde7fb437f94791be46
Christine Contreras <christineec23@yahoo.com>
2021-04-25 11:56:15 -0500

    DOMContentLoaded practice complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Code your solution in index.js. First, set up a DOMContentLoaded event listener to detect when the HTML page has loaded and the document is ready to be manipulated.
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    //Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+    const div = document.getElementById('text');
+    div.innerHTML = "This is really cool!";
+})
